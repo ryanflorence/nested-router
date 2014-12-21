8d9df04dd54f1d6a26597550eb9bcb440fcb88ce
Ryan Florence <rpflorence@gmail.com>
2014-12-21 11:59:29 -0700

    make mutable operations searchable

--- examples/react/utils/makeRouter.js
+++ examples/react/utils/makeRouter.js
@@ -1,9 +1,26 @@
-var React = require('react');
-var fetch = require('./fetch');
-var validateTransition = require('./validate');
 var { match } = require('../../../index');
+var React = require('react');
+var all = require('when/keys').all;
+
+var fetch = (routes, params) => {
+  return all(routes.filter((route) => {
+    return route.fetch;
+  }).reduce((promises, route) => {
+    promises[route.name] = route.fetch(params);
+    return promises;
+  }, {}));
+};
+
+var validateTransition = (routes, ref) => {
+  return !routes.length || routes.filter((route) => {
+    return route.validate;
+  }).reduceRight((valid, route) => {
+    return route.validate(ref);
+  }, true);
+};
 
 var makeRouter = module.exports = (routes, location) => {
+
   var transient = {
     ref: {},
     handlers: [],
@@ -11,14 +28,19 @@ var makeRouter = module.exports = (routes, location) => {
     path: ''
   };
 
+  var mutateTransient = (changes) => {
+    for (var key in changes)
+      transient[key] = changes[key];
+  };
+
   var restoreUrl = () => {
-    transient.aborted = true;
+    mutateTransient({ aborted: true });
     location.push(transient.path);
   };
 
   location.listen(() => {
     if (transient.aborted)
-      return (transient.aborted = false);
+      return mutateTransient({ aborted: false });
 
     if (!validateTransition(transient.handlers, transient.ref))
       return restoreUrl();
@@ -26,16 +48,14 @@ var makeRouter = module.exports = (routes, location) => {
     var path = location.getPath();
     var { handlers, params } = match(path, routes);
 
-    transient.handlers = handlers;
-    transient.path = path;
+    mutateTransient({ handlers, path });
 
     fetch(handlers, params).then((data) => {
-      var element = handlers.reduceRight((element, handler) => {
-        return handler.render(data, element);
+      var element = handlers.reduceRight((element, route) => {
+        return route.render(data, element);
       }, null);
-      transient.ref = React.render(element, document.body);
+      mutateTransient({ ref: React.render(element, document.body) });
     });
   });
 };
-
 
--- examples/react/utils/validate.js
+++
@@ -1,8 +0,0 @@
-var validate = module.exports = (routes, ref) => {
-  return !routes.length || routes.filter((route) => {
-    return route.validate;
-  }).reduceRight((valid, route) => {
-    return route.validate(ref);
-  }, true);
-};
-
